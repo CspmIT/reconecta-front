bbf67a608d3da3053cb3b3c16e4ca25c6b2db337
fabri075 <juanfabri69@hotmail.com>
2024-08-02 06:50:57 -0300

    fix: Carga de cambios para trabajar en casa

--- src/modules/substation/components/ChargeBoard/index.jsx
+++ src/modules/substation/components/ChargeBoard/index.jsx
@@ -7,7 +7,7 @@ const ChargeBoard = () => {
 	return (
 		<div className='flex flex-row justify-center w-full md:w-3/6 pt-5'>
 			<CardCustom className='w-full min-h-28 border-l-[1rem] border-r-2 border-b-2 border-red-500  shadow-md !rounded-lg overflow-hidden p-5'>
-				<h1 className='text-center'>DETALLE</h1>
+				<h1 className='text-center font-bold'>CARGA MÁXIMA</h1>
 				<div className='w-full flex md:flex-row p-3'>
 					<div className='w-full md:w-1/4 grid items-center md:max-w-1/4 bg-slate-200 m-1'>
 						<b className='w-full text-center'>TIPO</b>
--- src/modules/substation/components/ComunicationBoard/index.jsx
+++ src/modules/substation/components/ComunicationBoard/index.jsx
@@ -5,42 +5,28 @@ const ComunicationBoard = () => {
 	return (
 		<div className='flex flex-row justify-end w-full md:w-2/6 pt-5'>
 			<CardCustom className='w-full md:w-11/12 min-h-28 border-l-[1rem] border-r-2 border-b-2 border-red-500  shadow-md !rounded-lg overflow-hidden p-5'>
-				<h1 className='text-center font-bold'>VALORES INSTANTÁNEOS</h1>
-				<div className='w-full flex flex-wrap items-center justify-center p-3'>
-					<div className='w-full grid items-center bg-slate-200 p-1'>
-						<b className='w-full text-center'>Corrientes Generales</b>
-						<div className='flex flex-row w-full'>
-							<div className='w-1/2 grid items-center bg-slate-200 m-1'>
-								<b className='w-full text-center'>Fase 1</b>
-								<p className='w-full text-center'>0.0 A</p>
-							</div>
-							<div className='w-1/2 grid items-center bg-slate-200 m-1'>
-								<b className='w-full text-center'>Fase 2</b>
-								<p className='w-full text-center'>0.0 A</p>
-							</div>
-							<div className='w-1/2 grid items-center bg-slate-200 m-1'>
-								<b className='w-full text-center'>Fase 3</b>
-								<p className='w-full text-center'>0.0 A</p>
-							</div>
+				<h1 className='text-center font-bold'>COMUNICACIÓN</h1>
+				<div className='w-full flex md:flex-row p-3'>
+					<div className='w-full md:w-1/4 grid items-center md:max-w-1/4 bg-slate-200 m-1'>
+						<b className='w-full text-center'>Módulo</b>
+						<p className='w-full text-center'>Cooptech </p>
+					</div>
+					<div className='w-full md:w-1/4 grid items-center md:max-w-1/4 bg-slate-200 m-1'>
+						<b className='w-full text-center'>IP</b>
+						<p className='w-full text-center'>-</p>
+					</div>
+					<div className='w-full md:w-2/4 flex flex-col items-center justify-center md:max-w-2/4 m-1'>
+						<div className='w-full'>
+							<input type='checkbox' className='mr-2' />
+						</div>
+						<div>
+							<input type='checkbox' className='mr-2' />
 						</div>
 					</div>
-					<div className='w-full grid items-center bg-slate-200 mt-2 p-1'>
-						<b className='w-full text-center'>Tensiones Generales</b>
-						<div className='flex flex-row w-full'>
-							<div className='w-1/2 grid items-center bg-slate-200 m-1'>
-								<b className='w-full text-center'>Fase 1</b>
-								<p className='w-full text-center'>0.0 V</p>
-							</div>
-							<div className='w-1/2 grid items-center bg-slate-200 m-1'>
-								<b className='w-full text-center'>Fase 2</b>
-								<p className='w-full text-center'>0.0 V</p>
-							</div>
-							<div className='w-1/2 grid items-center bg-slate-200 m-1'>
-								<b className='w-full text-center'>Fase 3</b>
-								<p className='w-full text-center'>0.0 V</p>
-							</div>
-						</div>
-					</div>
+				</div>
+				<div className='w-full'>
+					<b>Observaciones:</b>
+					<div className='w-full h-full bg-slate-200 min-h-16'></div>
 				</div>
 			</CardCustom>
 		</div>
