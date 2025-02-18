857c3e6782bbde0a4a6bca9117077d2f24f0c239
romani22 <you@example.com>
2025-02-18 07:57:30 -0300

    feat: metrología analizadores

--- src/modules/analyzer/board/components/metrologyBoard/index.jsx
+++ src/modules/analyzer/board/components/metrologyBoard/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import RowsValues from './rowsValues'
 import styles from './styles.module.css'
+import { request } from '../../../../../utils/js/request'
+import { backend } from '../../../../../utils/routes/app.routes'
 
-const MetrologyBoard = () => {
+const MetrologyBoard = ({ analyzer }) => {
+	const [info, setInfo] = useState([])
+	const [loading, setLoading] = useState(true)
+	const getData = async () => {
+		try {
+			const body = {
+				brand: analyzer?.equipmentmodels?.name.toLowerCase(),
+				version: analyzer?.equipmentmodels?.brand.toLowerCase(),
+				serial: analyzer?.serial,
+			}
+			const { data } = await request(`${backend[`${import.meta.env.VITE_APP_NAME}`]}/Analyzer`, 'POST', body)
+			setInfo(data)
+			setLoading(false)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	useEffect(() => {
+		getData()
+	}, [])
 	return (
+		!loading &&
 		<div className='w-full flex flex-row flex-wrap justify-center'>
 			<div className='w-full flex flex-row mb-5'>
 				<div className='w-0 md:w-2/6'></div>
@@ -14,19 +36,19 @@ const MetrologyBoard = () => {
 					<h1 className='w-full md:w-1/4 text-center md:text-left text-xl text-black'>Tensión</h1>
 				</div>
 			</div>
-			<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-sky-100 md:bg-inherit' />
+			<RowsValues valueR={`${info?.f_0_v} V`} valueS={`${info?.f_1_v} V`} valueT={`${info?.f_2_v} V`} valueTotal='-' bg='bg-sky-100 md:bg-inherit' />
 			<div className='w-full md:w-2/6 md:my-2 flex flex-row p-2 md:p-0 bg-gray-300 md:bg-inherit flex-wrap justify-center'>
 				<div className='w-8/12'>
 					<h1 className='md:w-1/4 text-center md:text-left text-xl text-black'>Corriente</h1>
 				</div>
 			</div>
-			<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-gray-300 md:bg-inherit' />
+			<RowsValues valueR={`${info?.f_0_i} A`} valueS={`${info?.f_1_i} A`} valueT={`${info?.f_2_i} A`} valueTotal='-' bg='bg-sky-100 md:bg-inherit' />
 			<div className='w-full md:w-2/6 md:my-2 flex flex-row flex-wrap p-2 bg-sky-100 md:bg-inherit md:p-0 justify-center'>
 				<div className='w-8/12'>
 					<h1 className='md:w-1/4 text-center md:text-left text-xl text-black'>Cos Ø</h1>
 				</div>
 			</div>
-			<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-sky-100 md:bg-inherit' />
+			<RowsValues valueR={info?.cos_0} valueS={info?.cos_1} valueT={info?.cos_2} valueTotal={info?.cos_total} bg='bg-sky-100 md:bg-inherit' />
 			<div className='w-full flex flex-row flex-wrap justify-center p-2 md:p-0 bg-gray-300 md:bg-inherit'>
 				<div className='w-full md:w-2/6 my-2  flex flex-row flex-wrap justify-center'>
 					<div className='w-8/12'>
@@ -45,7 +67,7 @@ const MetrologyBoard = () => {
 						</div>
 					</div>
 				</div>
-				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-gray-200' />
+				<RowsValues valueR={`${info?.f_0_p} kW`} valueS={`${info?.f_1_p} kW`} valueT={`${info?.f_2_p} kW`} valueTotal={`${info?.pot_activa} kW`} bg='bg-gray-200' />
 				<div className='w-full md:w-2/6 md:py-2 flex flex-row flex-wrap justify-center'>
 					<div className='flex flex-row w-full md:w-6/12 p-2 md:p-0 bg-yellow-50 md:bg-inherit'>
 						<div className='w-1/12 text-center hidden md:block'>
@@ -57,7 +79,7 @@ const MetrologyBoard = () => {
 						</div>
 					</div>
 				</div>
-				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-yellow-50 md:bg-inherit' />
+				<RowsValues valueR={`${info?.f_0_q} kVAr`} valueS={`${info?.f_1_q} kVAr`} valueT={`${info?.f_2_q} kVAr`} valueTotal={`${info?.pot_reactiva} kVAr`} bg='bg-yellow-50 md:bg-inherit' />
 				<div className='w-full md:w-2/6 flex flex-row flex-wrap justify-center'>
 					<div className='flex flex-row w-full md:w-6/12 p-2 md:p-0 bg-gray-200 md:bg-inherit'>
 						<div className='w-1/12 text-center hidden md:block'>
@@ -68,7 +90,7 @@ const MetrologyBoard = () => {
 						</div>
 					</div>
 				</div>
-				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-gray-200 md:bg-inherit' />
+				<RowsValues valueR={`${info?.aparent_0} kVA`} valueS={`${info?.aparent_1} kVA`} valueT={`${info?.aparent_2} kVA`} valueTotal={`${info?.pot_aparente} kVA`} bg='bg-gray-200 md:bg-inherit' />
 			</div>
 
 			<div className='w-full'>
@@ -109,7 +131,7 @@ const MetrologyBoard = () => {
 						</div>
 					</div>
 				</div>
-				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-gray-200'/>
+				<RowsValues valueR={`${info?.f_0_a} kWh`} valueS={`${info?.f_1_a} kWh`} valueT={`${info?.f_2_a} kWh`} valueTotal={`${info?.ener_activa} kWh`} bg='bg-gray-200' />
 				<div className='w-full md:w-2/6 flex flex-row flex-wrap justify-center'>
 					<div className='flex flex-row w-full md:w-6/12 p-2 md:p-0 bg-yellow-50 md:bg-inherit'>
 						<div className='w-1/12 text-center hidden md:block'>
@@ -126,7 +148,7 @@ const MetrologyBoard = () => {
 						</div>
 					</div>
 				</div>
-				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-yellow-50 md:bg-inherit'/>
+				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal={`${info?.f_0_ain} kWh`} bg='bg-yellow-50 md:bg-inherit' />
 				<div className='w-full md:w-2/6 flex flex-row flex-wrap justify-center'>
 					<div className='flex flex-row w-full md:w-6/12 p-2 md:p-0 bg-gray-200'>
 						<div className='w-1/12 text-center hidden md:block'>
@@ -142,7 +164,7 @@ const MetrologyBoard = () => {
 						</div>
 					</div>
 				</div>
-				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-gray-200 md:bg-inherit'/>
+				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal={`${info?.f_0_aout} kWh`} bg='bg-gray-200 md:bg-inherit' />
 				<div className='w-full md:w-2/6 flex flex-row flex-wrap justify-center'>
 					<div className='flex flex-row w-full md:w-6/12 p-2 md:p-0 bg-gray-300 md:bg-inherit'>
 						<div className='w-1/12 text-center hidden md:block'>
@@ -168,7 +190,7 @@ const MetrologyBoard = () => {
 						</div>
 					</div>
 				</div>
-				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-yellow-50 md:bg-inherit'/>
+				<RowsValues valueR={`${info?.f_0_r} kVArh`} valueS={`${info?.f_1_r} kVArh`} valueT={`${info?.f_2_r} kVArh`} valueTotal={`${info?.ener_reactiva} kVArh`} bg='bg-yellow-50 md:bg-inherit' />
 				<div className='w-full md:w-2/6 flex flex-row flex-wrap justify-center'>
 					<div className='flex flex-row w-full md:w-6/12 p-2 md:p-0 bg-gray-200'>
 						<div className='w-0 md:w-1/12 text-center'></div>
@@ -183,7 +205,7 @@ const MetrologyBoard = () => {
 						</div>
 					</div>
 				</div>
-				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-gray-200 md:bg-inherit'/>
+				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal={`${info?.f_1_ain} kVArh`} bg='bg-gray-200 md:bg-inherit' />
 				<div className='w-full md:w-2/6 flex flex-row flex-wrap justify-center'>
 					<div className='flex flex-row w-full md:w-6/12 p-2 md:p-0 bg-yellow-50 md:bg-inherit'>
 						<div className='w-0 md:w-1/12 text-center'></div>
@@ -197,7 +219,7 @@ const MetrologyBoard = () => {
 						</div>
 					</div>
 				</div>
-				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal='-' bg='bg-yellow-50 md:bg-inherit'/>
+				<RowsValues valueR='-' valueS='-' valueT='-' valueTotal={`${info?.f_2_aout} kVArh`} bg='bg-yellow-50 md:bg-inherit' />
 			</div>
 		</div>
 
