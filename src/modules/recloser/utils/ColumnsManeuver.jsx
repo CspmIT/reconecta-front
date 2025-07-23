2538ef4af0baf3351a81e80cf8cfd935f961cac1
Empanadas <juanfabri69@hotmail.com>
2025-07-23 08:36:37 -0300

    fix: maniobras

--- src/modules/recloser/utils/ColumnsManeuver.jsx
+++ src/modules/recloser/utils/ColumnsManeuver.jsx
@@ -1,10 +1,10 @@
 export const columnManeuver = [
 	{
-		accessorFn: (originalRow) => new Date(originalRow.date), //convert to date for sorting and filtering
+		accessorFn: (originalRow) => new Date(originalRow.createdAt), //convert to date for sorting and filtering
 		size: 100,
 		id: 'date',
 		header: 'Fecha',
-		accessorKey: 'date',
+		accessorKey: 'createdAt',
 		filterVariant: 'date-range',
 		Cell: ({ cell }) => `${cell.getValue().toLocaleDateString()} ${cell.getValue().toLocaleTimeString()}`, // convert back to string for display
 	},
@@ -15,6 +15,7 @@ export const columnManeuver = [
 		size: 40,
 	},
 	{
+		accessorFn: (originalRow) => `${originalRow.user_create.first_name} ${originalRow.user_create.last_name}`,
 		header: 'Usuario',
 		accessorKey: 'user_name',
 		muiFilterTextFieldProps: { placeholder: 'Usuario' },
