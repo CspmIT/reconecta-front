590db2c93f053975a6110de47ddc38dc26bcdbbf
Empanadas <juanfabri69@hotmail.com>
2026-01-12 12:09:54 -0300

    feat: modal descargas, añadido macOS

--- src/modules/core/components/Footer/index.jsx
+++ src/modules/core/components/Footer/index.jsx
@@ -1,8 +1,17 @@
+import { getVersion } from "@tauri-apps/api/app"
+import { useEffect, useState } from "react"
+
 function Footer() {
+	const [version, setVersion] = useState(false)
 	const year = new Date().getFullYear()
+	useEffect(() => {
+		getVersion()
+			.then(setVersion)
+			.catch(() => setVersion(false))
+	}, [])
 	return (
 		<div className='absolute bottom-0 !h-16 flex justify-center items-center w-full z-50 bg-primary'>
-			<h1>Copyright © IT & Development - COOPMORTEROS {year}</h1>
+			<h1>Copyright © IT & Development - COOPMORTEROS {year} {version && (<>- Versión {version}</>)} </h1>
 		</div>
 	)
 }
