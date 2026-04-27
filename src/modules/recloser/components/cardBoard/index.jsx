444d07a87092cc37b2738d0545755dde8e4ed52e
Empanadas <juanfabri69@hotmail.com>
2026-04-27 14:34:26 -0300

    fix: hidden table in recloser events

--- src/modules/recloser/components/cardBoard/index.jsx
+++ src/modules/recloser/components/cardBoard/index.jsx
@@ -1,4 +1,4 @@
-import { Tab, Tabs, useMediaQuery } from '@mui/material'
+import { Tab, Tabs } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { FaChartArea, FaPowerOff, FaTachometerAlt } from 'react-icons/fa'
 import { BsFiles } from 'react-icons/bs'
@@ -9,7 +9,6 @@ import ManeuverBoard from '../maneuverBoard'
 
 function CardBoard({ onCardSelect, info }) {
 	const [selectedCardId, setSelectedCardId] = useState(1)
-	const isMobile = useMediaQuery('(max-width: 600px)')
 	const boardCards = [
 		{ id: 1, name: 'METROLOGÍA', icon: <FaTachometerAlt /> },
 		{ id: 2, name: 'EVENTOS', icon: <BsFiles /> },
@@ -27,9 +26,9 @@ function CardBoard({ onCardSelect, info }) {
 	const classTabs =
 		'!border-solid !border-gray-200 !rounded-l-xl !text-base !text-black !font-bold dark:!text-zinc-200 dark:!border-gray-700'
 	return (
-		<div className='w-full max-w-[94.5vw] flex flex-row rounded-xl overflow-hidden'>
+		<div className='w-full flex flex-row rounded-xl overflow-hidden'>
 			<Tabs
-				className='flex flex-col !w-10 border-r  border-r-white dark:border-r-slate-500'
+				className='flex flex-col !w-10 flex-shrink-0 border-r  border-r-white dark:border-r-slate-500'
 				value={selectedCardId - 1}
 				orientation='vertical'
 				indicatorColor='transparent'
@@ -39,11 +38,10 @@ function CardBoard({ onCardSelect, info }) {
 					<Tab
 						key={item.id}
 						style={{ minWidth: 0, height: '144px' }}
-						className={`w-full ${
-							selectedCardId == item.id
-								? '!bg-white dark:!bg-zinc-500 '
-								: '!bg-gray-300  dark:!bg-zinc-800 '
-						}   !border-t-2 !border-l-2 !border-b-2  min-w-0 ${classTabs} relative`}
+						className={`w-full ${selectedCardId == item.id
+							? '!bg-white dark:!bg-zinc-500 '
+							: '!bg-gray-300  dark:!bg-zinc-800 '
+							}   !border-t-2 !border-l-2 !border-b-2  min-w-0 ${classTabs} relative`}
 						onClick={() => handleCard(item.id)}
 						label={
 							<div
@@ -61,8 +59,7 @@ function CardBoard({ onCardSelect, info }) {
 				))}
 			</Tabs>
 			<div
-				style={{ maxWidth: isMobile ? '84%' : '' }}
-				className='w-11/12 md:w-full bg-white dark:bg-zinc-500 mt-0.5 flex justify-center items-center border-2 border-t-0 border-l-0 md:p-4 p-3 rounded-r-2xl border-zinc-200 dark:border-gray-800'
+				className='w-full min-w-0 overflow-x-auto bg-white dark:bg-zinc-500 mt-0.5 flex justify-center items-center border-2 border-t-0 border-l-0 md:p-4 p-3 rounded-r-2xl border-zinc-200 dark:border-gray-800'
 			>
 				{selectedCardId === 1 && <MetrologyBoard idRecloser={info?.recloser?.id || null} />}
 				{selectedCardId === 2 && <EventBoard idRecloser={info?.recloser?.id || null} />}
